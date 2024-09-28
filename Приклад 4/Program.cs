4077d1da6cd294457952d9af8fe1f027cd596dba
KostiantynKrasnostup <vladovskiy462@gmail.com>
2024-09-28 19:59:32 +0300

    Add files via upload

---
+++ Приклад 4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j;
+            string strValue;
+            int[] iArray1 = new int[10];
+            int[] iArray2 = new int[10];
+
+            // Заміна на правильний синтаксис для створення об'єктів StreamReader та StreamWriter
+            StreamReader sRead = new StreamReader("D:\\dat.txt");
+            StreamWriter sWrite = new StreamWriter("D:\\res.txt");
+
+            for (j = 0; j < 10; j++)
+            {
+                // Зчитування значення з файлу
+                strValue = sRead.ReadLine();
+
+                // Конвертація рядка в ціле число
+                iArray1[j] = Convert.ToInt32(strValue);
+
+                // Обчислення нового значення
+                iArray2[j] = 10 * iArray1[j];
+
+                // Форматування рядка для виводу на консоль
+                strValue = string.Format("\n {0,4:D} {1,6:D} {2,6:D}", j, iArray1[j], iArray2[j]);
+
+                // Вивід на консоль
+                Console.WriteLine(strValue);
+                Console.WriteLine();
+
+                // Запис у файл
+                sWrite.WriteLine(iArray2[j]);
+            }
+
+            // Закриття потоків зчитування та запису
+            sRead.Close();
+            sWrite.Close();
+
+            // Очікування натискання клавіші, щоб програма не закривалася одразу
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Приклад 6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, num, sum = 0;
+            Random rnd = new Random();
+            int[] iArray = new int[10];
+
+            // Заполнение массива случайными числами от 1 до 100
+            for (j = 0; j < 10; j++)
+            {
+                iArray[j] = rnd.Next(1, 101);
+            }
+
+            // Подсчет суммы четных элементов массива
+            for (j = 0; j < 10; j++)
+            {
+                num = Convert.ToInt32(iArray[j] % 2);
+                if (num == 0) sum += iArray[j];
+            }
+
+            // Вывод массива на экран
+            foreach (int jj in iArray)
+            {
+                Console.Write(" " + jj);
+            }
+
+            // Пустая строка для форматирования вывода
+            Console.WriteLine("\n\n");
+
+            // Вывод суммы четных элементов
+            Console.WriteLine("\n Сума парних елементів = " + sum);
+
+            // Ожидание нажатия клавиши перед завершением программы
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
